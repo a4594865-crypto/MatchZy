87490a256b380addaf72a6f44b6278f9e9a4aecf
a4594865-crypto <a4594865@gmail.com>
2026-01-13 23:03:11 +0800

    Update Utility.cs

--- Utility.cs
+++ Utility.cs
@@ -680,36 +680,41 @@ namespace MatchZy
             }
         }
 
-        private void CheckLiveRequired()
+       // 替換 Utility.cs L683 開始的整段函式
+public void CheckLiveRequired()
+{
+    if (IsTeamsReady() && IsSpectatorsReady())
+    {
+        if (startCountdownTimer == null)
         {
-            if (!readyAvailable || matchStarted) return;
-
-            // Todo: Implement a same ready system for both pug and match
-            int countOfReadyPlayers = playerReadyStatus.Count(kv => kv.Value == true);
-            bool liveRequired = false;
-            if (isMatchSetup)
-            {
-                if (IsTeamsReady() && IsSpectatorsReady())
+            remainingCountdown = 10;
+            startCountdownTimer = AddTimer(1.0f, () => {
+                if (remainingCountdown > 0)
                 {
-                    liveRequired = true;
+                    string color = (remainingCountdown <= 5) ? $"{ChatColors.Red}" : $"{ChatColors.Green}";
+                    Server.PrintToChatAll($"{chatPrefix} 所有人已準備！比賽將在 {color}{remainingCountdown}{ChatColors.Default} 秒後開始...");
+                    
+                    foreach (var p in Utilities.GetPlayers().Where(p => p.IsValid && !p.IsBot))
+                    {
+                        p.ExecuteClientCommand("play sounds/ui/not_ready.vsnd");
+                    }
+                    remainingCountdown--;
                 }
-            }
-            else if (minimumReadyRequired == 0)
-            {
-                if (countOfReadyPlayers >= connectedPlayers && connectedPlayers > 0)
+                else
                 {
-                    liveRequired = true;
+                    startCountdownTimer?.Kill();
+                    startCountdownTimer = null;
+                    
+                    foreach (var p in Utilities.GetPlayers().Where(p => p.IsValid && !p.IsBot))
+                    {
+                        p.ExecuteClientCommand("play sounds/ui/match_start.vsnd");
+                    }
+                    HandleMatchStart(); 
                 }
-            }
-            else if (countOfReadyPlayers >= minimumReadyRequired)
-            {
-                liveRequired = true;
-            }
-            if (liveRequired)
-            {
-                HandleMatchStart();
-            }
+            }, TimerFlags.REPEAT);
         }
+    }
+}
 
         private void HandleMatchStart()
         {
